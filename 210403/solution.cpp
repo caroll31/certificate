4e21f22348e1f54b2a76187b8a83b735dc759801
caroll31 <55760858+caroll31@users.noreply.github.com>
2021-04-03 18:55:02 +0900

    Create solution.cpp

---
+++ 210403/solution.cpp
@@ -0,0 +1,30 @@
+#define MAX_WORD_NUMBER 25000
+#define MAX_WORD_LENGTH 20
+
+int strlen(const char str[])
+{
+    int ret = 0;
+    while(str[ret])
+        ret++;
+
+    return ret;
+}
+
+void init()
+{
+
+}
+
+void inputWord(const char mInput[20])
+{
+
+}
+
+int recommand(const char mUser[20])
+{
+    return 0;
+}
+void banWord(const char mUser[20])
+{
+
+}
